a3c0b09809a4a871810398888dd008680cda2901
Sandaruwan Fonseka <54181927+sandaruwanfonseka@users.noreply.github.com>
2021-11-17 23:37:09 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,17 +1,28 @@
 <?php
 
-include 'connection.php';
+class connection{
+	
+	function names($n1,$n2){
+		
+		$connection = new mysqli("localhost","root","","f_yr_project");
+
+
+		if ($connection->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$sql_query = "INSERT INTO student (f_name,l_name) VALUES ('$n1', '$n2')";
 	
 
-	$sql_query = "INSERT INTO student (f__name,l_name) VALUES ('John', 'Doe')";
-	
-
-	if ($connection->query($sql_query) === TRUE) {
-		echo "New record created successfully";
-	} else {
-		echo "<br>Error: ".$connection->error;
+		if ($connection->query($sql_query) === TRUE) {
+		echo "New record created successfully";}
 	}
+}	
 
-$connection->close();
+	$obj=new connection();
+	
+	
+	$obj->names($_POST['packet_1'],$_POST['packet_2']);
+
 
 ?>
