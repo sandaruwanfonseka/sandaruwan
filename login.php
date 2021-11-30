04e7eeb19f24a741fdf388491f0819bc124adf07
Sandaruwan Fonseka <54181927+sandaruwanfonseka@users.noreply.github.com>
2021-11-30 23:18:42 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,9 @@
+<form action="check.php" method="POST">
+    
+    <input type="text" placeholder="Enter Username" name="uname"> <br><br>
+  
+    <input type="text" placeholder="Enter Password" name="psw"> <br><br>
+
+    <button type="submit">Login</button>
+    
+</form>
